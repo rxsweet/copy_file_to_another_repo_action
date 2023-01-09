a2ebbeef74424362074c9e44953e199b71daa608
rxsweet <105535100+rxsweet@users.noreply.github.com>
2023-01-09 11:52:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,10 +67,6 @@ RUN echo "${TZ}" > /etc/timezone \
 && apt install -y tzdata \ 
 && rm -rf /var/lib/apt/lists/*
 
-作者：六百万
-链接：https://juejin.cn/post/7082670118257295391
-来源：稀土掘金
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 echo "Adding git commit"
 git add .
