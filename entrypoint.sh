0da99b0c4b581e894cf1c28a9afb59684db1b42d
rxsweet <105535100+rxsweet@users.noreply.github.com>
2023-01-11 08:58:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -64,7 +64,7 @@ echo "Adding git commit"
 git add .
 if git status | grep -q "Changes to be committed"
 then
-  git commit --message "$(date '+%Y-%m-%d %H:%M:%S') $INPUT_COMMIT_MESSAGE"
+  git commit --message "$(date -d'+8 hour''+%Y-%m-%d %H:%M:%S') $INPUT_COMMIT_MESSAGE"
   echo "Pushing git commit"
   git push -u origin HEAD:"$OUTPUT_BRANCH"
 else
